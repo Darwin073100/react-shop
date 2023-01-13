40661820b3c73963c3e950e19a062da685492611
Darwin073100 <edwinegq@outlook.com>
2023-01-13 11:44:23 -0600

    Request to API => Hoks useEffect & useState

---
+++ README.md
@@ -0,0 +1,6 @@
+# Curso Práctico de React.js
+¡Convierte tus diseños en HTML y CSS a componentes reutilizables en React.js! Configura tu entorno de desarrollo con Webpack y Babel. Define los componentes, contenedores, rutas, react hooks de tu proyecto. Despliega tu proyecto a producción con Netlify. Conviértete en React Developer profesional con tu profesor Oscar Barajas.
+
+- Desarrollar una tienda online con React
+- Llevar un diseño de HTML y CSS a React
+- Crear tu primer sitio web con React.js
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products?limit=6&offset=1';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(()=>{
+		const getProduct = async()=>{
+			const response = await axios.get(API);
+			setProducts(response.data);
+		}
+		getProduct();
+	},[]);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map((product)=>(
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
